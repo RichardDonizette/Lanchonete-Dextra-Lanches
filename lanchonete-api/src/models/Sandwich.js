8829d0e2a7ced82749eae1e4041104d277ad2286
Richard Donizete <richard.donizette@gmail.com>
2018-10-27 15:41:01 -0200

    -Ajustes do objeto Sandwich

--- lanchonete-api/src/models/Sandwich.js
+++ lanchonete-api/src/models/Sandwich.js
@@ -1,18 +1,18 @@
 module.exports = {
     "Bacon": {
         "Name": "X-Bacon",
-        "ingredient": ["Bacon", "Hambúrguer de carne", "Queijo"]
+        "ingredients": ["Bacon", "Hambúrguer de carne", "Queijo"]
     },
     "Burger": {
         "Name": "X-Burger",
-        "ingredient": ["Hambúrguer de carne", "Queijo"]
+        "ingredients": ["Hambúrguer de carne", "Queijo"]
     },
     "Egg": {
         "Name": "X-Egg",
-        "ingredient": ["Ovo", "Hambúrguer de carne", "Queijo"]
+        "ingredients": ["Ovo", "Hambúrguer de carne", "Queijo"]
     },
     "EggBacon": {
         "Name": "X-Egg Bacon",
-        "ingredient": ["Bacon", "Ovo", "Hambúrguer de carne", "Queijo"]
+        "ingredients": ["Bacon", "Ovo", "Hambúrguer de carne", "Queijo"]
     }
 }
