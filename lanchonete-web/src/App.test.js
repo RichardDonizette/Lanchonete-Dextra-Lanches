4fe9a0ee95fccaf6c56e7f5ac53ae9f502a28166
Richard Donizete <richard.donizette@gmail.com>
2018-10-31 00:03:45 -0200

    -Adicionando Testes de renderização dos componentes;

--- lanchonete-web/src/App.test.js
+++ lanchonete-web/src/App.test.js
@@ -1,10 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { connect } from 'react-redux';
-import Home from './screens/Home';
 
-it('renders without crashing', () => {
+import Home from './screens/Home';
+import CreateSandwich from './screens/CreateSandwich';
+import SelectSandwich from './screens/SelectSandwich';
+import Sandwich from './components/Sandwich';
+import IngredientList from './components/IngredientList';
+import Header from './components/Header';
+import Cart from './components/Cart';
+import menu from './reducers/index'
+
+//Teste de render das telas
+
+it('renders Home without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(connect(<Home />), div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders CreateSandwich without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(connect(<CreateSandwich />), div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders SelectSandwich without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(connect(<SelectSandwich />), div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+//Teste de render dos Componentes
+
+it('renders component Sandwich without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(connect(<Sandwich />), div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders component IngredientList without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(connect(<IngredientList />), div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders component Header without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(connect(<Header />), div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders component Cart without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(connect(<Cart />), div);
   ReactDOM.unmountComponentAtNode(div);
 });
